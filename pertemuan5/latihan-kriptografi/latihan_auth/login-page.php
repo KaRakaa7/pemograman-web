ba3771774fddfcff45fdf7bfa21ad5614615d14c
KaRakaa7 <114058639+KaRakaa7@users.noreply.github.com>
2022-11-09 11:26:53 +0700

    pertemuan5
    password

---
+++ pertemuan5/latihan-kriptografi/latihan_auth/login-page.php
@@ -0,0 +1,32 @@
+<?php
+error_reporting(0);
+session_start();
+
+if(isset($_SESSION['username'])){
+    header('Location:admin.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" constant="width-device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="login.css">
+    <title>Login Page</title>
+</head>
+<body>
+    <div class="kotak_login">
+    <h2 class="tulisan_login">LOGIN</h3>
+    <form action="index.php" method="POST">
+        <label for="username">Username</label>
+        <input name="username" type="text" class="form_login" placeholder="Masukan username anda ....">
+        <br/>
+        <label for="password">Password</label>
+        <input name="password" type="password" class="form_login" placeholder="Masukan password anda ....">
+        <br/>
+        <input type="submit" name="submit" value="Login" class="login">
+    </form>
+    </div>
+</body>
+</html>
---
+++ pertemuan5/latihan-kriptografi/latihan_auth/register_process.php
@@ -0,0 +1,23 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $username = $_POST['username'];
+    $gender = $_POST['gender'];
+    $email = $_POST['email'];
+
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    date_default_timezone_set('Asia/Jakarta');
+    $created = date("Y-m-d H:i:s");
+
+    include_once("config.php");
+
+    $result = mysqli_query($conn_db, "INSERT INTO users(username,gender,email,password,created_at) VALUES ('$username','$gender','$password','$created')");
+
+    echo "Users " . $username . " added succesfully. <a href='login-page.php'>Login</a>";
+
+    if($result){
+        header( "refresh:5;url=login-page.php" );
+        echo 'Success Register, You will be redirected to login Page in 5 secs.';
+    }
+}
+?>
